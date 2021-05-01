044aa525eb49e2cd41f927373503c5180cd7d0a7
adilsonchameia <adilsonkchameia@outlook.com>
2021-05-01 21:06:04 +0100

    criada main view

---
+++ lib/input_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
+      ),
+      body: Center(
+        child: Text("Texto"),
+      ),
+    );
+  }
+}
