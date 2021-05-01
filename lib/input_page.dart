f0955bc027b074714b03b10cadc898fab24ce652
adilsonchameia <adilsonkchameia@outlook.com>
2021-05-01 22:31:14 +0100

    card principal refratorado

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,65 +18,57 @@ class _HomeState extends State<Home> {
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    //Borda
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                  child: ResuableCode(
+                    color: Color(0xFF1D1E33),
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    //Borda
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                  child: ResuableCode(
+                    color: Color(0xFF1D1E33),
                   ),
                 )
               ],
             ),
           ),
           Expanded(
-            child: Container(
-              margin: EdgeInsets.all(15.0),
-              //Borda
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
-          ),
+              child: ResuableCode(
+            color: Color(0xFF1D1E33),
+          )),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    //Borda
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
+                    child: ResuableCode(
+                  color: Color(0xFF1D1E33),
+                )),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    //Borda
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                )
+                    child: ResuableCode(
+                  color: Color(0xFF1D1E33),
+                ))
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ResuableCode extends StatelessWidget {
+  //Contrutor
+  //@required - Obrigatorio
+  ResuableCode({@required this.color});
+  //Cor No Selector
+  //A cor e imutavel, apos setar a cor, nao podemos setar novamente
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      //Borda
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
