a79f01e170ee8fc9dba620bf681f32e90adb069c
adilsonchameia <adilsonkchameia@outlook.com>
2021-05-02 22:46:12 +0100

    refratoracao icones nos cards

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -26,6 +26,21 @@ class _HomeState extends State<Home> {
                 Expanded(
                   child: ResuableCode(
                     color: corAtivada,
+                    cardChild: Column(
+                      children: [
+                        Icon(size: 80.0),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          "MASCULINO",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
@@ -65,16 +80,19 @@ class _HomeState extends State<Home> {
   }
 }
 
+//Widget - Container De Atividades
 class ResuableCode extends StatelessWidget {
   //Contrutor
   //@required - Obrigatorio
-  ResuableCode({@required this.color});
+  ResuableCode({@required this.color, this.cardChild});
   //Cor No Selector
   //A cor e imutavel, apos setar a cor, nao podemos setar novamente
   final Color color;
+  final Widget cardChild;
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       //Borda
       decoration: BoxDecoration(
